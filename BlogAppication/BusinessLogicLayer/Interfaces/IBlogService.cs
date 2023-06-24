84fcc96c7721702c014ad118bd9dd00aa1a42fcc
RostikRaiter <67313991+RostikRaiter@users.noreply.github.com>
2023-06-24 14:09:58 +0300

    update

---
+++ BlogAppication/BusinessLogicLayer/Interfaces/IBlogService.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Interfaces
+{
+    public interface IBlogService
+    {
+        Task<List<Blog>> GetBlogs();
+        Task<Blog> GetBlog(int? id);
+        Task CreateBlog(Blog blog);
+        Task EditBlog(Blog blog);
+        Task DeleteBlog(int? id);
+    }
+}
---
+++ BlogAppication/BusinessLogicLayer/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Interfaces
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetUsers();
+        Task<User> GetUser(string id);
+        Task CreateUser(User user);
+        Task EditUser(User user);
+        Task DeleteUser(string id);
+    }
+}
